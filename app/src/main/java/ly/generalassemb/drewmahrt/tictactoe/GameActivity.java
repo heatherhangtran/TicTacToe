f5d0a7293b20423a7edbdd222bf00864fc92c6bd
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-04-22 16:19:57 -0700

    Change TextView ids and declare them in Game Activity.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -1,13 +1,29 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    TextView aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree;
+    TextView[] mArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        aOne = (TextView) (findViewById(R.id.a1));
+        aTwo = (TextView) (findViewById(R.id.a2));
+        aThree = (TextView) (findViewById(R.id.a3));
+        bOne = (TextView) (findViewById(R.id.b1));
+        bTwo = (TextView) (findViewById(R.id.b2));
+        bThree = (TextView) (findViewById(R.id.b3));
+        cOne = (TextView) (findViewById(R.id.c1));
+        cTwo = (TextView) (findViewById(R.id.c2));
+        cThree = (TextView) (findViewById(R.id.c3));
+
+        mArray = new TextView[] {aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree};
+
     }
 }
