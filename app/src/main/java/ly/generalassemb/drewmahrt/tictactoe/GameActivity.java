70014276b8d54c90c90426db91736a0dc0fd90b2
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-04-22 16:42:51 -0700

    Change TextViews to Buttons.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -2,28 +2,28 @@ package ly.generalassemb.drewmahrt.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
-    TextView aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree;
-    TextView[] mArray;
+    Button aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree;
+    Button[] mArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        aOne = (TextView) (findViewById(R.id.a1));
-        aTwo = (TextView) (findViewById(R.id.a2));
-        aThree = (TextView) (findViewById(R.id.a3));
-        bOne = (TextView) (findViewById(R.id.b1));
-        bTwo = (TextView) (findViewById(R.id.b2));
-        bThree = (TextView) (findViewById(R.id.b3));
-        cOne = (TextView) (findViewById(R.id.c1));
-        cTwo = (TextView) (findViewById(R.id.c2));
-        cThree = (TextView) (findViewById(R.id.c3));
+        aOne = (Button) (findViewById(R.id.a1));
+        aTwo = (Button) (findViewById(R.id.a2));
+        aThree = (Button) (findViewById(R.id.a3));
+        bOne = (Button) (findViewById(R.id.b1));
+        bTwo = (Button) (findViewById(R.id.b2));
+        bThree = (Button) (findViewById(R.id.b3));
+        cOne = (Button) (findViewById(R.id.c1));
+        cTwo = (Button) (findViewById(R.id.c2));
+        cThree = (Button) (findViewById(R.id.c3));
 
-        mArray = new TextView[] {aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree};
+        mArray = new Button[] {aOne, aTwo, aThree, bOne, bTwo, bThree, cOne, cTwo, cThree};
 
     }
 }
