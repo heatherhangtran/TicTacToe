8b128271b8684bd7528037d3586b6cd7a7f59bef
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-04-24 15:05:43 -0700

    Hides keyboard after each name entry.

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/MainActivity.java
@@ -1,18 +1,22 @@
 package ly.generalassemb.drewmahrt.tictactoe;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText mPlayerOneEditText, mPlayerTwoEditText;
-    Button mButton;
-    TextView mTextView;
+    private EditText mPlayerOneEditText, mPlayerTwoEditText;
+    private Button mButton;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +47,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //The following two methods hides the keyboard after each entry.
+        //I AM NOT A FAN OF THE KEYBOARD. The keyboard shows only when needed.
+        //The EditText xml has singleLine to allow only one line of text.
+        //The following code changes the Enter button to hide the keyboard. *WOOT WOOT!*
+        mPlayerOneEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager hideKeyBoard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    hideKeyBoard.hideSoftInputFromWindow(mPlayerOneEditText.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mPlayerTwoEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    InputMethodManager hideKeyBoard = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    hideKeyBoard.hideSoftInputFromWindow(mPlayerTwoEditText.getWindowToken(), 0);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+
 }
